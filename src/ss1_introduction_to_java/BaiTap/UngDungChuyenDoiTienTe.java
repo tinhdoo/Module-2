e0cd1ce65125beb9354012e00fc09023b55493ad
tinhdoo <hotinh13102@gmail.com>
2025-05-08 16:18:36 +0700

    new

--- src/ss1_introduction_to_java/BaiTap/UngDungChuyenDoiTienTe.java
+++ src/ss1_introduction_to_java/BaiTap/UngDungChuyenDoiTienTe.java
@@ -1,2 +1,15 @@
-package ss1_introduction_to_java.BaiTap;public class UngDungChuyenDoiTienTe {
+package ss1_introduction_to_java.BaiTap;
+import java.util.Scanner;
+
+public class UngDungChuyenDoiTienTe {
+  public static void main(String[] args) {
+    double VND, USD;
+    final double rate = 26000;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter USD: ");
+    USD = scanner.nextDouble();
+
+    VND = USD * rate;
+    System.out.println(VND);
+  }
 }
--- src/ss1_introduction_to_java/BaiTap/UngDungDocSoThanhChu.java
+++ src/ss1_introduction_to_java/BaiTap/UngDungDocSoThanhChu.java
@@ -1,2 +1,24 @@
-package ss1_introduction_to_java.BaiTap;public class UngDungDocSoThanhChu {
+package ss1_introduction_to_java.BaiTap;
+
+import java.util.Scanner;
+
+public class UngDungDocSoThanhChu {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Nhập số cần đọc: ");
+    int read = scanner.nextInt();
+    switch (read) {
+      case 1 -> System.out.println("One");
+      case 2 -> System.out.println("Two");
+      case 3 -> System.out.println("Three");
+      case 4 -> System.out.println("four");
+      case 5 -> System.out.println("five");
+      case 6 -> System.out.println("Six");
+      case 7 -> System.out.println("Seven");
+      case 8 -> System.out.println("Eight");
+      case 9 -> System.out.println("Nine");
+      case 10 -> System.out.println("Ten");
+      default -> System.out.println("Out of ability");
+    }
+  }
 }
